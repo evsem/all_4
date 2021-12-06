5ad020cda1c12e2407cb9514f52b4d519431a1e6
evsem <frukwork@gmail.com>
2021-12-06 22:54:10 +0300

    v(Classes_1)-all_4

---
+++ js/Classes.js
@@ -0,0 +1,9 @@
+class Info {
+  constructor(name, age, hasChild) {
+    this.name = name
+    this.age = age
+    this.hasChild = hasChild
+  }
+}
+let infoStudent = new Info('Alex', 22, true)
+console.log(infoStudent)
